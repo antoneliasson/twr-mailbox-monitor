25590b111c1335d7313726d22e60229ba6c8bb0a
Anton Eliasson <devel@antoneliasson.se>
2022-12-30 15:26:23 +0100

    Delete humidity tag handler
    Turns out that the tag holders on the encoder module cannot be used if a
    cover is placed under the LCD module. That leaves the holders on the
    power module, of which there are only two.

--- src/sensors.c
+++ src/sensors.c
@@ -2,7 +2,6 @@
 
 static twr_tag_temperature_t temperature_tag;
 static twr_tag_barometer_t barometer_tag;
-static twr_tag_humidity_t humidity_tag;
 
 void temperature_tag_event_handler(twr_tag_temperature_t *self, twr_tag_temperature_event_t event, void *event_param)
 {
@@ -37,22 +36,6 @@ void barometer_tag_event_handler(twr_tag_barometer_t *self, twr_tag_barometer_ev
     }
 }
 
-void humidity_tag_event_handler(twr_tag_humidity_t *self, twr_tag_humidity_event_t event, void *event_param)
-{
-    if (event == TWR_TAG_HUMIDITY_EVENT_UPDATE)
-    {
-        float percentage;
-
-        twr_tag_humidity_get_humidity_percentage(self, &percentage);
-        twr_log_debug("APP: humidity: %.1f", percentage);
-        twr_radio_pub_humidity(TWR_RADIO_PUB_CHANNEL_R2_I2C0_ADDRESS_DEFAULT, &percentage);
-    }
-    else if (event == TWR_TAG_HUMIDITY_EVENT_ERROR)
-    {
-        twr_log_error("APP: hygrometer error");
-    }
-}
-
 void sensors_init(void)
 {
     twr_tag_temperature_init(&temperature_tag, TWR_I2C_I2C0, TWR_TAG_TEMPERATURE_I2C_ADDRESS_DEFAULT);
@@ -62,8 +45,4 @@ void sensors_init(void)
     twr_tag_barometer_init(&barometer_tag, TWR_I2C_I2C0);
     twr_tag_barometer_set_event_handler(&barometer_tag, barometer_tag_event_handler, NULL);
     twr_tag_barometer_set_update_interval(&barometer_tag, 10000);
-
-    twr_tag_humidity_init(&humidity_tag, TWR_TAG_HUMIDITY_REVISION_R2, TWR_I2C_I2C0, TWR_TAG_HUMIDITY_I2C_ADDRESS_DEFAULT);
-    twr_tag_humidity_set_event_handler(&humidity_tag, humidity_tag_event_handler, NULL);
-    twr_tag_humidity_set_update_interval(&humidity_tag, 10000);
 }
