23d46fd4f68ba0d2129372e93bdc813e5f8b2860
Anton Eliasson <devel@antoneliasson.se>
2022-12-31 10:55:31 +0100

    Re-add humidity tag and ignore tag error events
    My barometer tag is broken apparently so now I have a slot for a
    humidity tag again.

--- src/sensors.c
+++ src/sensors.c
@@ -2,6 +2,7 @@
 
 static twr_tag_temperature_t temperature_tag;
 static twr_tag_barometer_t barometer_tag;
+static twr_tag_humidity_t humidity_tag;
 
 void temperature_tag_event_handler(twr_tag_temperature_t *self, twr_tag_temperature_event_t event, void *event_param)
 {
@@ -12,10 +13,6 @@ void temperature_tag_event_handler(twr_tag_temperature_t *self, twr_tag_temperat
         twr_tag_temperature_get_temperature_celsius(self, &celsius);
         twr_log_debug("APP: temperature: %.2f °C", celsius);
         twr_radio_pub_temperature(TWR_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &celsius);
-    }
-    else if (event == TWR_TAG_TEMPERATURE_EVENT_ERROR)
-    {
-        twr_log_error("APP: Thermometer error");
     }
 }
 
@@ -30,9 +27,17 @@ void barometer_tag_event_handler(twr_tag_barometer_t *self, twr_tag_barometer_ev
         twr_log_debug("APP: pressure: %.2f hPa, altitude: %.2f m", pascal / 100, meter);
         twr_radio_pub_barometer(TWR_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &pascal, &meter);
     }
-    else if (event == TWR_TAG_BAROMETER_EVENT_ERROR)
+}
+
+void humidity_tag_event_handler(twr_tag_humidity_t *self, twr_tag_humidity_event_t event, void *event_param)
+{
+    if (event == TWR_TAG_HUMIDITY_EVENT_UPDATE)
     {
-        twr_log_error("APP: barometer error");
+        float percentage;
+
+        twr_tag_humidity_get_humidity_percentage(self, &percentage);
+        twr_log_debug("APP: humidity: %.1f %% rel", percentage);
+        twr_radio_pub_humidity(TWR_RADIO_PUB_CHANNEL_R2_I2C0_ADDRESS_DEFAULT, &percentage);
     }
 }
 
@@ -45,4 +50,8 @@ void sensors_init(void)
     twr_tag_barometer_init(&barometer_tag, TWR_I2C_I2C0);
     twr_tag_barometer_set_event_handler(&barometer_tag, barometer_tag_event_handler, NULL);
     twr_tag_barometer_set_update_interval(&barometer_tag, 10000);
+
+    twr_tag_humidity_init(&humidity_tag, TWR_TAG_HUMIDITY_REVISION_R2, TWR_I2C_I2C0, TWR_TAG_HUMIDITY_I2C_ADDRESS_DEFAULT);
+    twr_tag_humidity_set_event_handler(&humidity_tag, humidity_tag_event_handler, NULL);
+    twr_tag_humidity_set_update_interval(&humidity_tag, 10000);
 }
